c28af88c9d34df8e744d3a6f373712ca528c416b
Armaan Dhanoa <armaandhanoa@Armaans-Mini.lan>
2021-05-18 12:31:53 -0500

    course finished

---
+++ course/src/components/dynamicComponent.js
@@ -0,0 +1,5 @@
+const DynamicComponent = () => {
+    return <p>This text was not rendered on the server</p>
+}
+
+export default DynamicComponent
---
+++ course/theme.js
@@ -0,0 +1,27 @@
+import { roboto } from '@theme-ui/presets'
+
+const theme = {
+  ...roboto,
+//   colors: {
+//       ...roboto.colors,
+//       background: 'yellow'
+//   },
+  containers: {
+    card: {
+      boxShadow: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+      border: '1px solid',
+      borderColor: 'muted',
+      borderRadius: '4px',
+      p: 2,
+    },
+    page: {
+      width: '100%',
+      m: 0,
+      mx: 'auto',
+    }
+  },
+  styles: {
+    ...roboto.styles
+  }
+}
+export default theme
